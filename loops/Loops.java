a438bbc73df6d525f9645e8f3b0b492cc92b9d22
Ege Uysal <hi@egeuysal.com>
2025-11-25 22:16:17 -0600

    Learn loops

--- loops/Loops.java
+++ loops/Loops.java
@@ -1,6 +1,25 @@
 package loops;
 
+import java.util.Scanner;
+
 public class Loops {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+    int number = scanner.nextInt();
+    scanner.close();
+
+    for (int i = 0; i <= number; i++) {
+      System.out.println(i);
+    }
+    
+    System.out.println("First loop completed!");
+
+    while (number > 0) {
+      System.out.println(number);
+      number--;
+    }
+  }
 }
