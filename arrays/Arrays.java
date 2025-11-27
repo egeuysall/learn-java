61f79d6580b493c1cdff5ffda7ada16deb911375
Ege Uysal <hi@egeuysal.com>
2025-11-26 20:50:59 -0600

    Finish arrays

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -1,6 +1,37 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class Arrays {
 
-  public static void main(String[] args) {}
+  public static int add(int[] nums) {
+    int count = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      count += nums[i];
+    }
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("How many numbers: ");
+    int num = scanner.nextInt();
+    scanner.nextLine();
+
+    int[] nums = new int[num];
+
+    for (int i = 0; i < nums.length; i++) {
+      System.out.printf("Enter number %d: ", i + 1);
+      int userNum = scanner.nextInt();
+      nums[i] = userNum;
+    }
+
+    scanner.close();
+
+    int result = add(nums);
+
+    System.out.println(result);
+  }
 }
